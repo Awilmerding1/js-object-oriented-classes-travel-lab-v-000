e13681a71b401ea5ce5f200c4066251c720d9b6f
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-04 17:14:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+class Driver {
+  
+  constructor(name, day) {
+    this.name = name 
+    this.startDate = new Date(day)
+  }
+  
+  yearsExperienceFromBeginningOf(year) {
+    return year - (this.startDate.getFullYear()) - 1
+  }
+  
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation 
+    this.endingLocation = endingLocation
+  }
+  
+  blocksTravelled() {
+    let h = this.beginningLocation[0] - this.endingLocation[0]
+    let v = this.beginningLocation[1]
+  }
+  
+}
