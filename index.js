48f192b819ef040a75d79f12d5c6183b23abbe15
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-04 17:48:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ class Route {
     let hStart = Number.parseInt(this.beginningLocation['vertical'], 10) 
     let hEnd = Number.parseInt(this.endingLocation['vertical'], 10)
     
+    let h= ""
     let v = ""
     
     if (hStart < hEnd) {
