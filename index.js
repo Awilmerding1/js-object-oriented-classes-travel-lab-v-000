a699f2cd114b2bbc2a9c95790b6a12cbf3f271df
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-04 17:29:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ class Route {
   blocksTravelled() {
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
     
-    let hStart = Number.parseInt(this.beginningLocation[0], 10) 
-    let hEnd = Number.parseInt(this.endingLocation[0], 10)
+    let hStart = Number.parseInt(this.beginningLocation[1], 10) 
+    let hEnd = Number.parseInt(this.endingLocation[1], 10)
     
     if (hStart < hEnd) {
       let h = hEnd - hStart
@@ -30,9 +30,9 @@ class Route {
       let h =  hStart - hEnd
     }
     
-    let vStart = eastWest.findIndex(this.beginningLocation[1])
+    let vStart = eastWest.findIndex(this.beginningLocation[0])
     
-    let vEnd = eastWest.findIndex(this.endingLocation[1])
+    let vEnd = eastWest.findIndex(this.endingLocation[0])
     
     if (vStart < vEnd) {
       let v = vEnd - vStart
